0fd23ef2ae8f13f476b797de42c19580abe9dfc6
adrian <acedillo99@gmail.com>
2024-08-19 13:46:45 -0600

    url correction

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -10,34 +10,37 @@ type Props = {
 
 const CodeSnippet = ({ id }: Props) => {
   const { toast } = useToast()
+  
+  const deploymentUrl = "https://maria-sales.vercel.app"; // Store the URL in a variable for easy updates
+
   let snippet = `
     const iframe = document.createElement("iframe");
     
     const iframeStyles = (styleString) => {
-    const style = document.createElement('style');
-    style.textContent = styleString;
-    document.head.append(style);
+      const style = document.createElement('style');
+      style.textContent = styleString;
+      document.head.append(style);
     }
     
-    iframeStyles('
+    iframeStyles(\`
         .chat-frame {
             position: fixed;
             bottom: 50px;
             right: 50px;
             border: none;
         }
-    ')
+    \`)
     
-    iframe.src = "http://localhost:3000/chatbot"
+    iframe.src = "${deploymentUrl}/chatbot"
     iframe.classList.add('chat-frame')
     document.body.appendChild(iframe)
     
     window.addEventListener("message", (e) => {
-        if(e.origin !== "http://localhost:3000") return null
+        if(e.origin !== "${deploymentUrl}") return null
         let dimensions = JSON.parse(e.data)
         iframe.width = dimensions.width
         iframe.height = dimensions.height
-        iframe.contentWindow.postMessage("${id}", "http://localhost:3000/")
+        iframe.contentWindow.postMessage("${id}", "${deploymentUrl}/")
     })
         `
 
