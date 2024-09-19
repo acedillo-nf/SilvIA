a9096645f51d89628a7bf4a9ea5dd90e9198c3e3
AlexNavarrow <33789077+Navarrow0@users.noreply.github.com>
2024-09-18 20:15:59 -0600

    public api middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,22 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/auth(.*)',"/integraciones",'/contacto', '/precios', '/nosotros','/api(.*)', '/api/twilio', '/api/twilio/[userId]', ],
+  publicRoutes: [
+    '/',
+    '/auth(.*)',
+    "/integraciones",
+    '/contacto',
+    '/precios',
+    '/nosotros',
+    '/api/twilio',
+    '/api/twilio/[userId]',
+  ],
 });
 
 export const config = {
   matcher: [
-    '/', 
+    '/',
     '/((?!_next/static|_next/image|favicon.ico|api|trpc).*)',
+    '/api/twilio/(.*)'
   ],
 };
-
