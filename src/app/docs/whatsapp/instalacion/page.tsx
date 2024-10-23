2f9c328859fda805ec317c2a8dc1776398dadc1b
adrian <acedillo99@gmail.com>
2024-10-23 13:34:42 -0600

    correcion

--- src/app/docs/whatsapp/instalacion/page.tsx
+++ src/app/docs/whatsapp/instalacion/page.tsx
@@ -9,8 +9,8 @@ const DocsPage = () => {
         <h2 className="text-2xl font-semibold mb-4">Crear plantillas de WhatsApp</h2>
         <ol className="list-decimal list-inside space-y-4">
           <li>Ir a: <a href="https://business.facebook.com/wa/manage/message-templates/" className="text-blue-600 hover:underline">https://business.facebook.com/wa/manage/message-templates/</a></li>
-          <li>Haga clic en: "Crear plantilla"</li>
-          <li>Elija la opción Utilizar una plantilla en blanco haciendo clic en "Crear plantilla".</li>
+          <li>Haga clic en: &quot;Crear plantilla&quot;</li>
+          <li>Elija la opción Utilizar una plantilla en blanco haciendo clic en &quot;Crear plantilla&quot;.</li>
         </ol>
 
         <div className="mt-4">
@@ -20,7 +20,7 @@ const DocsPage = () => {
             <li>Nombre: (utilice un nombre descriptivo)</li>
             <li>Cabecera: Comienzo del mensaje</li>
             <li>Cuerpo: Mensaje principal</li>
-            <li>Botones: Siempre es recomendable añadir el botón "Desactivar Marketing" porque WhatsApp valora que respondamos correctamente a las peticiones de "No Molestar" de los usuarios.</li>
+            <li>Botones: Siempre es recomendable añadir el botón &quot;Desactivar Marketing&quot; porque WhatsApp valora que respondamos correctamente a las peticiones de &quot;No Molestar&quot; de los usuarios.</li>
           </ul>
         </div>
 
@@ -31,11 +31,11 @@ const DocsPage = () => {
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-4">Para iniciar una nueva conversación desde Gloria AI</h2>
         <ol className="list-decimal list-inside space-y-4">
-          <li>Vaya a "Conversaciones" -&gt; "Nueva conversación".</li>
-          <li>Actualice la lista de plantillas haciendo clic en "Actualizar".</li>
+          <li>Vaya a &quot;Conversaciones&quot; -&gt; &quot;Nueva conversación&quot;.</li>
+          <li>Actualice la lista de plantillas haciendo clic en &quot;Actualizar&quot;.</li>
           <li>Seleccione la plantilla que desea enviar</li>
           <li>Escriba el número de teléfono</li>
-          <li>Y luego haga clic en "Enviar".</li>
+          <li>Y luego haga clic en &quot;Enviar&quot;.</li>
         </ol>
       </section>
     </div>
